b186782787b4fdc90e7eb785f2191af2063f5633
Andrian Dev <andrianpoint@gmail.com>
2022-09-12 11:35:07 +0700

    Change bootstrap cdn to npm package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+// import { useEffect } from 'react';
 import Layout from '@/layout/main';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // useEffect(() => {
+  //   import('bootstrap/dist/js/bootstrap');
+  // }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
